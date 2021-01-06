f1bf183b54b0cdf38c592ecef830f736a4aedfb0
alam.kurniawan <alam.kurniawan@mncgroup.com>
2021-01-06 11:07:04 +0700

    add facebook

--- config/config.go
+++ config/config.go
@@ -13,6 +13,6 @@ const (
 	RedisHost string = "localhost"
 	RedisPort string = "6379"
 
-	DataBlokPath string = "/var/www/html/datablok/drugs.txt"
-	BlokCategory string = "4"
+	DataBlokPath string = "/var/www/html/datablok/facebook.txt"
+	BlokCategory string = "5"
 )
