49f5a5cd3422c36436d34b934bc2c72c3206b669
alam.kurniawan <alam.kurniawan@mncgroup.com>
2021-01-06 12:56:15 +0700

    add phising

--- config/config.go
+++ config/config.go
@@ -13,6 +13,6 @@ const (
 	RedisHost string = "localhost"
 	RedisPort string = "6379"
 
-	DataBlokPath string = "/var/www/html/datablok/malware.txt"
-	BlokCategory string = "8"
+	DataBlokPath string = "/var/www/html/datablok/phising.txt"
+	BlokCategory string = "9"
 )
