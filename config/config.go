4aa88e0dacd7f6730c0157258162044554133988
alam.kurniawan <alam.kurniawan@mncgroup.com>
2021-01-06 11:13:40 +0700

    add fraud

--- config/config.go
+++ config/config.go
@@ -13,6 +13,6 @@ const (
 	RedisHost string = "localhost"
 	RedisPort string = "6379"
 
-	DataBlokPath string = "/var/www/html/datablok/facebook.txt"
-	BlokCategory string = "5"
+	DataBlokPath string = "/var/www/html/datablok/fraud.txt"
+	BlokCategory string = "6"
 )
